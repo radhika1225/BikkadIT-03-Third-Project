6f5b746ac798c187ab3267f793c5d7587913a5b9
Your Name <you@example.com>
2022-08-07 09:39:21 +0530

    Third project added to git repository

---
+++ src/test/java/com/example/GitThirdProject/GitThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitThirdProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
